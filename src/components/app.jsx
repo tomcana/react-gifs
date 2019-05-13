92dd33591615eb4e75da4b58b8667effc8c887fc
Tomomi Shiba <follwthedot@gmail.com>
2019-05-13 20:17:48 +0900

    search bar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar.jsx';
 
 class App extends Component {
   render() {
     return(
       <div>
-        <div className="left-scene"></div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+
+          </div>
+        </div>
         <div className="right-scene"></div>
       </div>
     );
